011f3ba218573695782c0254193be8253e9f59d8
Sadaku1993 <Sadalu1993@users.noreply.github.com>
2018-05-30 23:24:21 +0900

    update

--- src/odom_pictogram.cpp
+++ src/odom_pictogram.cpp
@@ -7,6 +7,32 @@
 
 ros::Publisher pub;
 
+void odom_Callback(const nav_msgs::OdometryConstPtr& msg)
+{
+    jsk_rviz_plugins::Pictogram pictogram;
+    pictogram.header.frame_id = msg->header.frame_id;
+    pictogram.header.stamp = ros::Time::now();
+
+    pictogram.pose.position.x = msg->pose.pose.position.x;
+    pictogram.pose.position.y = msg->pose.pose.position.y;
+    pictogram.pose.position.z = 1.5;
+    pictogram.pose.orientation.x = 0.7;
+    pictogram.pose.orientation.y = 0;
+    pictogram.pose.orientation.z = 0.7;
+    pictogram.pose.orientation.w = 0;
+
+    pictogram.action = pictogram.ROTATE_X;
+    pictogram.mode   = pictogram.PICTOGRAM_MODE;
+    pictogram.character = "fa-wifi";
+    pictogram.size = 1;
+    pictogram.speed = 2.0;
+    pictogram.color.r = 25/255.0;
+    pictogram.color.g = 255/255.0;
+    pictogram.color.b = 240/255.0;
+    pictogram.color.a = 1.0;
+    pub.publish(pictogram);
+}
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "odom_pictogram");
@@ -14,37 +40,10 @@ int main(int argc, char** argv)
     
     ros::Rate rate(20);
 
+    ros::Subscriber sub = n.subscribe("/odom", 1, odom_Callback);
     pub = n.advertise<jsk_rviz_plugins::Pictogram>("/odom/pictogram", 1);
 
-    while(ros::ok())
-    {
-        jsk_rviz_plugins::Pictogram pictogram;
-        pictogram.header.frame_id = "map";
-        pictogram.header.stamp = ros::Time::now();
-
-        pictogram.pose.position.x = 0;
-        pictogram.pose.position.y = 0;
-        pictogram.pose.position.z = 1.0;
-        pictogram.pose.orientation.x = 0.7;
-        pictogram.pose.orientation.y = 0;
-        pictogram.pose.orientation.z = 0.7;
-        pictogram.pose.orientation.w = 0;
-        
-        pictogram.action = pictogram.ROTATE_X;
-        pictogram.mode   = pictogram.PICTOGRAM_MODE;
-        pictogram.character = "fa-wifi";
-        pictogram.size = 1;
-        pictogram.speed = 2.0;
-        pictogram.color.r = 25/255.0;
-        pictogram.color.g = 255/255.0;
-        pictogram.color.b = 240/255.0;
-        pictogram.color.a = 1.0;
-        pub.publish(pictogram);
-
-
-        ros::spinOnce();
-        rate.sleep();
-    }
+    ros::spin();
 
     return 0;
 }
